d59634a936996f9d6b30926fa5b2bf2b947b4465
ioparnas <ioparnas@uth.gr>
2011-11-14 13:14:33 +0200

    second attempt

---
+++ ccV2/src/web/CurConv.java
@@ -0,0 +1,23 @@
+package web;
+
+import model.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import com.google.gson.*;
+
+public class CurConv extends HttpServlet {
+	public void doPost(HttpServletRequest request,
+	HttpServletResponse response)
+	throws IOException, ServletException {
+		String c1 = request.getParameter("From");
+		String c2 = request.getParameter("To");
+		String c3 = request.getParameter("Money");
+		CurExpert ce = new CurExpert();
+		String result = ce.getMoney(c1,c2,c3);
+		request.setAttribute("result", result);
+		RequestDispatcher view = request.getRequestDispatcher("index.jsp");
+		view.forward(request, response);
+	}
+}
